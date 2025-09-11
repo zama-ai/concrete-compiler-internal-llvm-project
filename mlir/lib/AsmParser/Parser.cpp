be9bcfa017ade27a2ad730d67981416326d5cda2
Andi Drebes <andi.drebes@zama.ai>
2025-09-11 10:29:51 +0200

    [mlir][parser] Fix creation of placeholder entries createForwardRefPlaceholder for null types
    The function `resolveOperands` in `GLWEOpsParsers.cpp` deliberately
    erases the types of all operands of `glwe.call` operations to avoid
    type clashes before type propagation. However, for undefined SSA
    values, the subsequent invocation of `Parser::resolveOperands()`
    reuses the erased type when creating a placeholder operation and
    passes it to the builder of `builtin.unresolved_conversion_cast`. This
    leads to the creation of a TypeRange for the operands of the
    placeholder operation with a `null` type, ultimately triggering an
    assertion of the constructor of `TypeRange`.
    
    This workaround sets the type for the placeholder operation to a bogus
    integer type in order to create a valid type range and to let the
    creation of the placeholder operation succeed.

--- mlir/lib/AsmParser/Parser.cpp
+++ mlir/lib/AsmParser/Parser.cpp
@@ -1096,6 +1096,13 @@ Value OperationParser::createForwardRefPlaceholder(SMLoc loc, Type type) {
   // cannot be created through normal user input, allowing us to distinguish
   // them.
   auto name = OperationName("builtin.unrealized_conversion_cast", getContext());
+  // Use bogus type if type is null to satisfy the constructor of the
+  // TypeRange created for the placeholder op
+  if (!type) {
+    type = IntegerType::get(getContext(), 42,
+                            mlir::IntegerType::SignednessSemantics::Unsigned);
+  }
+
   auto *op = Operation::create(
       getEncodedSourceLocation(loc), name, type, /*operands=*/{},
       /*attributes=*/std::nullopt, /*successors=*/{}, /*numRegions=*/0);
